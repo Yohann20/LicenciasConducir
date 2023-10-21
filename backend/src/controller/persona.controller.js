24b1cfb0733f1c1a39e508e4f7d011dd4582e92b
Dylan <dylan.suazo2101@alumnos.ubiobio.cl>
2023-10-21 16:03:48 -0300

    Cree funcion obtenerPersonas en personas.controller

--- backend/src/controller/persona.controller.js
+++ backend/src/controller/persona.controller.js
@@ -1,5 +1,11 @@
 import Persona from '../models/persona.modelo.js'; 
 
+
+export async function obtenerPersonas(req, res){
+    const personas = await Persona.find();
+    res.json(personas);
+}
+
 export async function crearPersona(req, res){ 
     const { nombre, rut, email, contraseña, roles } = req.body; 
     const nuevaPersona = new Persona({ nombre, rut, email, contraseña, roles }); 
